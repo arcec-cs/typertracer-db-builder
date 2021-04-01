b460329697d8751ab4f64f6e71923970e2bdc04c
arcec-cs <arcec.cs@gmail.com>
2021-03-31 22:19:25 -0400

    revamped traverse.js to meet needs of Ink Project: made into module getTextFilePathIndex(rootDirectory, metaDataJSON, language) that returns a pathIndex object containing Keys:textId, Values:filePath; The PG-Text-FileSystem contains one or many encoding of each text and has texts in various languages. Ink currently only wants english texts, and one text-file per text. The traverse module achives this by recursively traversing the rootDirectory, using regex to check if the path is a text-file and gets it's id, uses metaDataJSON index to check the language by id, and adds the path to the pathIndex if a text with an alternate encoding has not been already added.

--- traverse.js
+++ traverse.js
@@ -1,18 +1,63 @@
-const path = require("path");
-const fs  = require("fs");
-const util = require('util');
-let files  = [];
+const PATH = require("path");
+const FS  = require("fs");
 
-regexTxt = /(.txt)$/
+const regexTxt = /([0-9]+.txt)$/; // gets only txt files, excludes read.me.txt included in some indexes
+const regexTxtNum = /[0-9]+(?=\\[0-9]*-*[0-9]+.txt)/; // get text num from path
 
-function recDirectoryTraverse(dir) {
-    fs.readdirSync(dir).forEach(file => {
-        const absPath = path.join(dir, file);
-        if (fs.statSync(absPath).isDirectory()) return recDirectoryTraverse(absPath);
-        else if (regexTxt.test(absPath)) return files.push(absPath);
-    });
+const pathToMeta = "..//data//pg-metadata//gutenberg-metadata.txt";
+const metaJSON = FS.readFileSync(pathToMeta, "utf-8");
+const pgMeta= JSON.parse(metaJSON);
+
+let created = false;
+
+const textFilePathIndex = {};
+
+function recursiveDirectoryTraversal(dir, lang) {
+  FS.readdirSync(dir).forEach(file => {
+       const path = PATH.join(dir, file);
+       if (FS.statSync(path).isDirectory()) return recursiveDirectoryTraversal(path, lang);
+       else if (isTextFile(path) && isLanguage(path, lang)) return checkFilePathIndex(path, getTextId(path));
+   });
+ }
+
+function isTextFile(path){
+  return regexTxt.test(path);
 }
 
-recDirectoryTraverse("./pg-txt");
+function isTextFilePathIndexCreated(){
+  return created; 
+}
 
-console.log(util.inspect(files, { maxArrayLength: null }));
+function isLanguage(path, lang){
+  return (getTextLanguage(path) == lang ? true : false); 
+}
+
+function getTextLanguage(path){
+  return pgMeta[getTextId(path)].language[0];
+}
+
+function getTextId(path){
+  return path.match(regexTxtNum)[0];
+}
+
+function existsInTextFilePathIndex(id){
+  return (textFilePathIndex[id] !== undefined ? true : false); 
+}
+
+function checkFilePathIndex(path, id){
+  if(!existsInTextFilePathIndex(id)) addToTextFilePathIndex(path, id);
+}
+
+function addToTextFilePathIndex(path, id){
+  textFilePathIndex[id]= path;
+}
+
+function createTextFilePathIndex(rootDir, lang){
+  recursiveDirectoryTraversal(rootDir, lang);
+  created = true;
+  return textFilePathIndex;
+}
+
+export default function getTextFilePathIndex(rootDir, lang){
+  return (isTextFilePathIndexCreated() ? textFilePathIndex : createTextFilePathIndex(rootDir, lang)); 
+}
