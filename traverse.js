3257681ad2c06e9f28cc6f970486aad1f67a07b4
arcec-cs <arcec.cs@gmail.com>
2021-04-06 18:48:53 -0400

    depricated traverse.getTextFilePathIndex() for traverse.createTextFilePathIndexJson(): creates a json file containg textFilePathIndex since pg-text file system is static/ waste to compute more than once. Included logic to: create local directory data to put json in, check if json is already created so it isnt computed needlessly and to log module progress/ if json already exists to the user.

--- traverse.js
+++ traverse.js
@@ -4,11 +4,16 @@ const FS  = require("fs");
 const regexTxt = /([0-9]+.txt)$/; // gets only txt files, excludes read.me.txt included in some indexes
 const regexTxtNum = /[0-9]+(?=\\[0-9]*-*[0-9]+.txt)/; // get text num from path
 
-let created = false;
+let metaDataIndex; //arg of module export that is an indexed object correlating with the indexed filesystem of the arg rootDir
+const textFilePathIndex = {}; //the output 
 
-let metaDataIndex;
+const dataDirectory = './/data';
+const jsonName = 'textFilePathIndex.json';
+const jsonPath = `${dataDirectory}//${jsonName}`;
 
-const textFilePathIndex = {};
+const existsMsg = `traverse.js: NO NEW JSON FILE CREATED, ${jsonName} already exists at '${jsonPath}'. if the rootDir has been updated please expell json and rerun, else ignore this message.`;
+const successMsg = `traverse.js: ${jsonPath} created.`; 
+const dirCreateMsg =`traverse.js: ${dataDirectory} created`;
 
 function recursiveDirectoryTraversal(dir, lang) {
   FS.readdirSync(dir).forEach(file => {
@@ -19,8 +24,6 @@ function recursiveDirectoryTraversal(dir, lang) {
  }
 
 const isTextFile = path => regexTxt.test(path);
-
-const isTextFilePathIndexCreated = () => created;
 
 const isLanguage = (path, lang) => (getTextLanguage(path) == lang ? true : false);
 
@@ -34,15 +37,24 @@ const checkFilePathIndex = (path, id) => !existsInTextFilePathIndex(id) && addTo
 
 const addToTextFilePathIndex = (path, id) => textFilePathIndex[id]= path;
 
+const createJson = obj => { FS.writeFileSync(jsonPath, JSON.stringify(obj)); console.log(successMsg); };
+
+const createDataDir = () => FS.mkdirSync(dataDirectory);
+
+const dataDirectoryExits = () => { FS.existsSync(dataDirectory); console.log(dirCreateMsg); };
+
+const jsonExists = () => FS.existsSync(jsonPath);
+
 function createTextFilePathIndex(rootDir, metaData, lang){
   metaDataIndex = metaData;
+  !dataDirectoryExits() && createDataDir();
+  console.log(`traverse.js: Creation of ${jsonPath} in progress...`);
   recursiveDirectoryTraversal(rootDir, lang);
-  created = true;
   return textFilePathIndex;
 }
 
-const getTextFilePathIndex = (rootDir, metaData,  lang) => (isTextFilePathIndexCreated() ? textFilePathIndex : createTextFilePathIndex(rootDir, metaData, lang));
+const createTextFilePathIndexJson = (rootDir, metaData,  lang) => !jsonExists() ? createJson(createTextFilePathIndex(rootDir, metaData, lang)) : console.log(existsMsg);
 
 module.exports ={
-  getTextFilePathIndex: getTextFilePathIndex
+  createTextFilePathIndexJson: createTextFilePathIndexJson
 }; 
