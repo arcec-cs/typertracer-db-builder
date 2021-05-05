b8c2fc1b89539c1e0a088a2bfd110ec37080dfab
arcec-cs <arcec.cs@gmail.com>
2021-05-04 20:07:52 -0400

    Exports FF createMetaDataSubjectIndex(idArr) which is a subtype of IdIndex; MDSIndex{id:{subjectlist:[],subjectPriority{}}}. subjectList subjects are from gutenberg-metadata.json. subjectPriority{} is created from matching each ids subjectList values with RegexPriority consts and shall contain a property for each corresponing Regex it matches; propertysubject0 ... subject3 (high...low). also exports a dependencies object with helper funcs/prop

---
+++ metaDataSubjectFrequencyIndex.js
@@ -0,0 +1,73 @@
+////MetaDataSubjectFrequencyIndex.js(MDSFI) - exports Fact Func that creates MDSFI containg 
+//Specific and NonSpecific subjectFrequencyIndex MDFSI:{specific:{}, nonSpecific:{}} w/ keys: subj, values: Frequency #(unique per id)
+// unique i.e if id 1 contains subject "Fiction" twice only adds one to "Fiction, Frequency"    
+
+//imports
+const MDSIndex = require("./metaDataSubjectIndex");
+
+//HELPERS: bool, regex, setters, sort
+//bool
+const isMultiValued = (subject) => /,/.test(subject); // Specific subject sometimes multivalued i.e Epic poetry, English (Old) -- ..
+const hasOnlySpecific = (subject) => !(/\-\-/.test(subject));
+const hasTrailingSpace = (subject) => regexTrailingSpace.test(subject);//BANDAID for regexNonSpecific
+
+//regex
+const regexSpecific =/[\(\)\,\.\w\s]*(?=\s\-)/; //matches word whitespace and spec chars
+const regexNonSpecific = /(?<=\-\-\s)[\(\)\,\w\s]*/g; //global to match I to N !!FIX TRAILING SPACE IN MIDDLE SUB!!
+const regexTrailingSpace = /\s$/; // BANDAID until better regexNonSpecific 
+
+//setters
+const setTempIndex = (index, subject) =>{ //helper for Sp and NSp
+  (index[subject] === undefined) && (index[subject]=1); //only counts a unique subject once per Id
+} 
+const setTempIndexSp = (index, subjectSp) => { //sub spec: cheks multi, adds each multi or single val
+  (isMultiValued(subjectSp)) ? 
+    subjectSp.split(',').forEach(subject => setTempIndex(index, subject)) : 
+    setTempIndex(index, subjectSp);
+}
+const setTempIndexNSp = (index, subjectN) => { // sub Non spec
+  subjectN.forEach(subject => {
+    if(hasTrailingSpace(subject)) subject = subject.replace(regexTrailingSpace, ""); //BANDAID UNTIL BETTER REGEX 
+    setTempIndex(index, subject);
+  });
+}
+const setIndex = (index, tempHolder, nameHolder) => { //sets return index from temp index each iteration
+  nameHolder.forEach(subjName => Object.keys(tempHolder[subjName]).forEach(subj => { //iterate nameHolder to set each temp prop 
+    (index[subjName][subj]==undefined) ? 
+      index[subjName][subj] = 1 : 
+      index[subjName][subj]++;
+  }));
+}
+//sort
+const sortByFrequency = (frequencyIndex) => Object.entries(frequencyIndex).sort((a,b) =>b[1]-a[1]); //returns array, sorts by freq
+
+//FACTORY FUNCTION: creates specific and nonSpecific FrequencyIndex; keys: Sp/NonSp subject, values: number of times it appears in UNIQUE Ids(Frequency)
+const createMetaDataSubjectFrequencyIndex = (MDSIndexBase) => { 
+  //TypeCheck 
+  if (!MDSIndex.dependencies.isMetaDataSubjectIndex(MDSIndexBase)) //MDSIndex contains, keys: id values: subjectList
+    throw new TypeError("MUST PROVIDE A MetaDataSubjectIndex as first arg");
+  
+  const index = {specific:{}, nonSpecific:{}}; //SubjectList value format (specific "-- non-specificI"... "-- non-specificN")(Animals -- Fiction --..)  
+  
+  // Iterate subjectList for each Id, parse Sp and NSp /w regex, set to index
+  MDSIndexBase.ids.forEach(id =>{
+    const tempHolder = {nonSpecific:{}, specific:{}}; //only unique subj per texts, refresh 
+    MDSIndexBase[id].subjectList.forEach(subject =>{ 
+      if (hasOnlySpecific(subject)) setTempIndexSp(tempHolder.specific, subject); //no need to parse value only ("specific")
+      else { //need to parse Sp from NSp and set temp
+        let subjectSpecific =  subject.match(regexSpecific)[0]; //[0] bc global flag
+        let subjectNonSpecific = subject.match(regexNonSpecific); 
+        setTempIndexSp(tempHolder.specific, subjectSpecific);
+        setTempIndexNSp(tempHolder.nonSpecific, subjectNonSpecific);
+      }
+    });
+    setIndex(index, tempHolder, ['specific', 'nonSpecific']);
+  });
+index.specific =  sortByFrequency(index.specific); //sort Sp
+index.nonSpecific =  sortByFrequency(index.nonSpecific); //sort NSp
+return index; 
+}
+
+module.exports= {
+  createMetaDataSubjectFrequencyIndex: createSubjectFrequencyIndex,
+};
---
+++ metaDataSubjectIndex.js
@@ -0,0 +1,70 @@
+////MetaDataSubjectIndex(MDSIndex) - Factory Func for a MDSIndex which contains Text's subject array from gutenberg-metadata.json refered to as subjectList
+// and a regex made subjectPriority object; MDSIBase only subject List. MDSIndex{ id:{subjectList:[], subjectPriority:{}} }
+//subjectPriority{subject1: HighS , subject2: MHighS,  subject3: MedS, subject4: LowS} property gets created on match, only matches MHishS subjectPriority(subject2: MHighS,) 
+
+const DATA = require("./data/staticDataStore.js");
+const IdIndex = require("./IdIndex.js");
+
+//regexSubjectPriority to parse subjects from subjectList;   
+const RegexSubjectHigh = /[A-Z][a-z]+\sfiction/; // higest priority for is the most broad yet descriptive subject category, tell ficiton and genere, most bang for buck 
+const RegexSubjectMedHigh = /((?<=\-\-\s)(Fiction|Biography|History|Poetry|Drama|Tragedies|Humor|Dictionaries))/gi;// these are broadCategories  tooSpecificCategory -- broadCategory i.e Animals -- Ficiton  Broadest 
+const RegexSubjectMed = /(Translations|poetry|drama|comedies|Philosophy|Politics|Political|Folklore|Short stories|Cooking|utopias|Biology|ethics|African Americans|Architecture)/gi; //Broad
+const RegexSubjectLow = /(Military|Drawing|Essays|travel|Fairy Tales|Mythology|occultism|sabotage|Christianity|Magic|Love|Latin language|sabotage|Church|Utilitarianism|Early works to 1800|Rizal, José|New Thought|Antiquities|Philippine|Steam-boilers|NeedleWork|Jews|United States|Legends|Imprisonment|Civil Disobedience|Biblical|Communism|lynching)/gi; //least broad, many just added to list to grab a subject for a text
+//Bible|Comidies|Economics|
+const createSubjectPriorityMatchesArr = subject => [ // dynamically stores matches for each regexSubjectPriority
+  subject.match(RegexSubjectHigh), 
+  subject.match(RegexSubjectMedHigh), 
+  subject.match(RegexSubjectMed), 
+  subject.match(RegexSubjectLow)
+];
+
+//PROTO/Helper
+const isSubjectPriorityObjSet = MDSIndex => //Helper
+  (MDSIndex[MDSIndex.ids[0]].subjectPriority != undefined); // subjectPriority created when set
+
+const metaDataSubjectIndexFunctionStore = { //proto
+  setSubjectPriorityObj : function () {
+    if(isSubjectPriorityObjSet(this)) return console.log("MDSIndex subjectPriority Object already set!");
+    this.ids.forEach(id => { 
+      this[id].subjectPriority = {};//set  
+      this[id].subjectList.forEach(subject => 
+        createSubjectPriorityMatchesArr(subject).forEach((match,ind) => //apply each regexSubjectPriority to a subject
+          (match !== null) && (this[id].subjectPriority['subject'+ ind] = match[0]))); //if match set associated priority property, take last 
+    });
+  },
+}
+
+//Factory Functions/Helper
+const setSubjectList = MDSIndex => MDSIndex.ids.forEach(id => //Helper 
+    MDSIndex[id].subjectList = DATA.PG_META_INDEX[id].subject);
+
+const createMetaDataSubjectIndexBase = idArray =>{ //FF: includes subjectList
+  let MDSIndexB = IdIndex.createIdIndex(idArray, metaDataSubjectIndexFunctionStore);
+  setSubjectList(MDSIndexB);
+  return MDSIndexB;
+}
+
+const createMetaDataSubjectIndex = idArray => { //FF: subject priority obj and/derrived from subjectList
+  let MDSIndex = createMetaDataSubjectIndexBase(idArray);
+  MDSIndex.setSubjectPriorityObj();
+  return MDSIndex;
+}
+
+//export
+const isMetaDataSubjectIndex = obj => //helper
+  (obj && Object.getPrototypeOf(obj) == metaDataSubjectIndexFunctionStore) ? true : false; // checks for arg and type
+
+module.exports= {
+  createMetaDataSubjectIndexBase: createMetaDataSubjectIndexBase,
+  createMetaDataSubjectIndex: createMetaDataSubjectIndex,
+  dependencies:{
+    isMetaDataSubjectIndex: isMetaDataSubjectIndex,
+    isSubjectPriorityObjSet: isSubjectPriorityObjSet,
+    numOfPriorities: createSubjectPriorityMatchesArr.length
+  }
+};
+
+
+
+
+
