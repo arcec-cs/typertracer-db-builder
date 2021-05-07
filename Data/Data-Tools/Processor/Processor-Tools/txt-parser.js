4c5ed93cdb74ae78572c68792cb768cec3792e54
arcec-cs <arcec.cs@gmail.com>
2021-05-07 01:14:32 -0400

    changed txt-parser.js from being written procedurally to partally functionally indempotente but not immutable. Changed text parsing return from array[#p][#s][#w] to array[#p][#s] for the added overhead/abilities are not needed. Also added a getNumOfWords Function since it is related to text parsing.

--- Data/Data-Tools/Processor/Processor-Tools/txt-parser.js
+++ Data/Data-Tools/Processor/Processor-Tools/txt-parser.js
@@ -1,31 +1,55 @@
-const fs = require('fs');
-const util = require('util');
+//text-parser.js partial functional implementation; is indempotent/uses pipe, but no need for data to be immutable; 
+const FS = require('fs');
 
+//regex
 const regexNonContBeg = /[\s\S]*?(?=(?<=\*{2,})[\r\n]{3,})/ // (?<=\*{2,})[\r\n]{3,} : match paragraph spacing if preceded by 2 or many \*, [\s\S]*?(?=X): match any and all char(non-greedy)if before X
 const regexNonContEnd = /(?<=[\r\n]{3,}(?=\*{2,}))[\s\S]*/ // [\r\n]{3,}(?=\*{2,}) : match paragraph spacing if followed by 2 or many \*, (?<= X)[\s\S]*: matach any and all char if after X
 const regexEnter = /\r\n/g; //Enter for windows machines
 const regexPara = /[\r\n]{3,}/; //for "paragraph delimiters" 
 const regexSen = /(?<=\.|\!|\?)\s/; //for any end of sentence punctuation
 
-const senArray = [];
+//helpers
+const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
 
-let text = fs.readFileSync("./text/frankenstein.txt", "utf-8"); // read a text file and save as string
+const txtToStringText = filePathToTxtFile => FS.readFileSync(filePathToTxtFile, "utf-8");
 
-text = text.replace(regexNonContBeg, '');// get rid of PG info Beg
-text = text.replace(regexNonContEnd, '');// get rid of PG info End
+const parseNonContentBeg = stringText => stringText.replace(regexNonContBeg, '');// get rid of PG info Beg; uses '*** START OF THE PROJECT GUTENBERG EBOOK FRANKENSTEIN ***' as delimeter
+const parseNonContentEnd = stringText => stringText.replace(regexNonContEnd, '');//get rid of PG info END; uses '*** END OF THE PROJECT GUTENBERG EBOOK FRANKENSTEIN ***' as delimeter
+const parseEnterEncoding = paraArray => { // parses out Enter encoding(\r\n) and adds space
+  paraArray.forEach((para,ind)=>{ paraArray[ind] = para.replace(regexEnter, ' '); }); // we do not need Enter encoding(\r\n) anymore, get rid of and add space
+  return paraArray; 
+}
 
-const paraArray = text.split(regexPara) //parses text into paragaphs and puts them into an array
+const splitToParaArr = (stringText) => stringText.split(regexPara); //split by para delimeter.
+const splitToParaSenArr = (paraArr) =>{
+  const senArray = [];
+  paraArr.forEach((para, ind)=>{ senArray[ind] = para.split(regexSen); }); // split into sentences by EOS punctuation senArr[#p][#sentence string]
+  return senArray;
+}
 
-paraArray.forEach((para,ind)=>{ paraArray[ind] = para.replace(regexEnter, ' '); }); // we do not need Enter encoding(\r\n) anymore, get rid of and add space
+//output notes. Sentences begin with a whitespace except begginging of paragraphs. quotation marks are escaped i.e (["A said\"I am a quote\"])
+const txtFileToParagraphSentenceArray = filePathToTxtFile => {  //returned array of element resutling in jagged array array[#p][#s] 
+  return pipe(
+    txtToStringText,
+    parseNonContentBeg,
+    parseNonContentEnd,
+    splitToParaArr,
+    parseEnterEncoding,
+    splitToParaSenArr
+  )(filePathToTxtFile)
+}
 
-paraArray.forEach((para, ind)=>{ senArray[ind] = para.split(regexSen); }); // split into sentences by EOS punctuation senArr[#p][#sentence string]
-
-senArray.forEach((para, indP)=>{ //index paragraphs
-  para.forEach((sen, indS)=>{ //index sentences
-    senArray[indP][indS] = sen.split(/\s/g); //split sentence string by space, return array of element resutling in jagged senArray[#p][#s][#w] 
+const getNumOfWords = paragraphSentenceArray => {
+  let numOfWords = 0;
+  paragraphSentenceArray.forEach( para => { //index paragraphs
+    para.forEach(sen => { //index sentences
+       numOfWords = numOfWords + (sen.split(/\s/g).length); //split sentence string by space, 
+    });
   });
-});
+  return numOfWords;
+};
 
-let bookJSON = JSON.stringify(senArray); 
-
-console.log(util.inspect(bookJSON, { maxArrayLength: null }));
+module.exports= {
+  txtFileToParagraphSentenceArray: txtFileToParagraphSentenceArray,
+  getNumOfWords: getNumOfWords
+};
