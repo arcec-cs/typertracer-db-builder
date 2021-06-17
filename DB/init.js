335018e98437111933f24e9cb670a1dc346df7ec
arcec-cs <arcec.cs@gmail.com>
2021-06-17 14:27:53 -0400

    changed db name to new database name refecting new project name from ink to typetracer

--- DB/init.js
+++ DB/init.js
@@ -1,20 +1,20 @@
-// init.js - initializes connection to and contents of db ink
+// init.js - initializes connection to and contents of db typetracer
 const knex = require('knex');
 const inkSchema = require('./DB-Init-Tools/schema');
 const inkInsert = require('./DB-Init-Tools/insert');
 
-//initialize connection to postgres db 'ink'
+//initialize connection to postgres db 'typetracer'
 const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
     user : 'postgres',
     password : 'test',
-    database : 'ink'
+    database : 'typetracer'
   }
 });
 
 inkSchema.createSchema(db) //create schema first
  .then(() => inkInsert.initAll(db))//init data
- .then(()=> console.log("init.js: Ink Database Intialized !")); //close connection 
+ .then(()=> console.log("init.js: typetracer Database Intialized !")); //close connection 
  //.then(()=> db.destroy()) errors, manual closing is okay since is a "user run script"
