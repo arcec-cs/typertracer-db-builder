a851fd6163eb455b4881e9aed28ab07bebd27e30
arcec-cs <arcec.cs@gmail.com>
2021-06-20 13:48:14 -0400

    changed name of texts from ink-texts{id}.json to {id}.json for simplicity

--- Data/Data-Tools/Processor/processorTexts.js
+++ Data/Data-Tools/Processor/processorTexts.js
@@ -14,13 +14,13 @@ const processTexts = () => {
 
   //Pre-Processing / set base path / 
   PRE_PROCESSOR.preProcess(); // creates necessary 'textFilePathIndex.json' if not already created
-  const inkTextPath =  PATH.resolve(__dirname,'..//..//Data-Processed//ink-data//ink-texts//ink-text');
+  const inkTextPath =  PATH.resolve(__dirname,'..//..//Data-Processed//ink-data//ink-texts');
   const inkIdIndex = ID_INDEX.createIdIndex(DATA.IDS_INPUT);// dont need for storge of props, but obj gives available ids form our id input.
 
   //Text Processing
   inkIdIndex.ids.forEach(id => { //IDS_INPUT FROM our list of text ids that we want to process  
     // set path for id
-    let idPath = `${inkTextPath}${id}.json`;
+    let idPath = `${inkTextPath}//${id}.json`;
     
     // check if exists
     if (jsonExists(idPath)) return console.log(`processorTexts.js: ${idPath} ALREADY EXISTS! if you would like to re-process please delte file, else ignore`)
