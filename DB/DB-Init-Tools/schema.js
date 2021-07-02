b978604fd60ff7ffd6720be7a3d67fde862775f6
arcec-cs <arcec.cs@gmail.com>
2021-07-01 23:38:05 -0400

    Updated schema to include pages in table "Texts" removed columns that we do not utilize.

--- DB/DB-Init-Tools/schema.js
+++ DB/DB-Init-Tools/schema.js
@@ -4,42 +4,40 @@ async function createSchema(db) {
     await db.schema.createTable('Authors', function (table) {
       table.increments('id');//pk default
       table.string('author', 120).unique().notNullable();
-      table.timestamps(false, true);
+      table.timestamp("created_at").defaultTo(db.fn.now());
     });
     await db.schema.createTable('Categories', function (table) {
       table.increments('id');//pk default
       table.string('category', 120).unique().notNullable();
-      table.timestamps(false, true);
+      table.timestamp("created_at").defaultTo(db.fn.now());
     });
     await db.schema.createTable('Users', function (table) {
       table.increments('id');//pk default
-      table.string('name', 60).notNullable();
+      table.string('name', 40).notNullable();
       table.string('email', 150).unique().notNullable();
-      table.bigInteger('words').notNullable().defaultTo(0);
-      table.specificType('avatar_id', 'smallInt').defaultTo(0);
-      table.timestamps(false, true);
+      table.timestamp("created_at").defaultTo(db.fn.now());
     });
     await db.schema.createTable('Login', function (table) {
       table.increments('id');//pk default
       table.string('email', 150).unique().notNullable();
       table.string('hash', 100).notNullable();
-      table.timestamp('updated_at');
+      table.timestamp("created_at").defaultTo(db.fn.now());
     });
     await db.schema.createTable('Texts', function (table) {
       table.integer('id').primary();//pk default
       table.string('title', 600).notNullable();
-      table.integer('words').notNullable();
+      table.specificType('pages', 'smallInt').notNullable();
       table.integer('author_id').references('id').inTable("Authors");
       table.integer('category_id').references('id').inTable("Categories");
-      table.timestamps(false, true);
+      table.timestamp("created_at").defaultTo(db.fn.now());
     });
     await db.schema.createTable('User_Texts', function (table) {
       table.integer('u_id').references('id').inTable("Users").onDelete("CASCADE"); // if user del no need to keep records
       table.integer('t_id').references('id').inTable("Texts");//no cascade, yet
       table.primary(['u_id','t_id']);//composite primary
-      table.specificType('progress','json');
-      table.integer('last_accessed').notNullable();
-      table.integer('words');   
+      table.specificType('progress','json').notNullable();
+      table.integer('last_accessed').notNullable();  
+      table.timestamp("created_at").defaultTo(db.fn.now()); 
     });
     console.log('created ink schema!');
    }
