0f4dadd972cca364776d2769e1c36d1c5489a2a1
arcec-cs <arcec.cs@gmail.com>
2021-07-16 22:23:44 -0400

    added heroku confiuration to initalize data to herok data store

--- DB/init.js
+++ DB/init.js
@@ -2,19 +2,22 @@
 const knex = require('knex');
 const inkSchema = require('./DB-Init-Tools/schema');
 const inkInsert = require('./DB-Init-Tools/insert');
-
+pgconfig = {rejectUnauthorized: false }
 //initialize connection to postgres db 'typetracer'
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'test',
-    database : 'typetracer'
+    connectionString: 'your heroku connection string here',
+    ssl: pgconfig, 
+    // for local config
+    // host : '127.0.0.1',
+    // user : 'postgres',
+    // password : 'test',
+    // database : 'typetracer'
   }
 });
 
-inkSchema.createSchema(db) //create schema first
+ inkSchema.createSchema(db) //create schema first
  .then(() => inkInsert.initAll(db))//init data
  .then(()=> console.log("init.js: typetracer Database Intialized !")); //close connection 
  //.then(()=> db.destroy()) errors, manual closing is okay since is a "user run script"
