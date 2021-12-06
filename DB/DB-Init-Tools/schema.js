85000966fb138f738ae0697c80786ba1ee1711c4
arcec-cs <arcec.cs@gmail.com>
2021-12-05 20:20:34 -0500

    Table names in a RDB are singular

--- DB/DB-Init-Tools/schema.js
+++ DB/DB-Init-Tools/schema.js
@@ -1,17 +1,17 @@
 //schema.js - module that creates table to initialize typetracer db
 async function createSchema(db) {
   try {
-    await db.schema.createTable('Authors', function (table) {
+    await db.schema.createTable('Author', function (table) {
       table.increments('id');//pk default
       table.string('author', 120).unique().notNullable();
       table.timestamp("created_at").defaultTo(db.fn.now());
     });
-    await db.schema.createTable('Categories', function (table) {
+    await db.schema.createTable('Category', function (table) {
       table.increments('id');//pk default
       table.string('category', 120).unique().notNullable();
       table.timestamp("created_at").defaultTo(db.fn.now());
     });
-    await db.schema.createTable('Users', function (table) {
+    await db.schema.createTable('User', function (table) {
       table.increments('id');//pk default
       table.string('name', 40).notNullable();
       table.string('email', 150).unique().notNullable();
@@ -23,7 +23,7 @@ async function createSchema(db) {
       table.string('hash', 100).notNullable();
       table.timestamp("created_at").defaultTo(db.fn.now());
     });
-    await db.schema.createTable('Texts', function (table) {
+    await db.schema.createTable('Text', function (table) {
       table.integer('id').primary();//pk default
       table.string('title', 600).notNullable();
       table.specificType('pages', 'smallInt').notNullable();
@@ -31,7 +31,7 @@ async function createSchema(db) {
       table.integer('category_id').references('id').inTable("Categories");
       table.timestamp("created_at").defaultTo(db.fn.now());
     });
-    await db.schema.createTable('User_Texts', function (table) {
+    await db.schema.createTable('User_Text', function (table) {
       table.integer('u_id').references('id').inTable("Users").onDelete("CASCADE"); // if user del no need to keep records
       table.integer('t_id').references('id').inTable("Texts");//no cascade, yet
       table.primary(['u_id','t_id']);//composite primary
